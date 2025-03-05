165031f4ceec3a8b6bc2691f56a5d853d5375b65
nitayg <31283093+nitayg@users.noreply.github.com>
2025-03-05 16:28:20 +0200

    Update supabase-client.ts

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -4,7 +4,7 @@ import { Album, Sticker, User, ExchangeOffer } from './types';
 
 // יצירת לקוח Supabase
 export const supabaseUrl = 'https://xdvhjraiqilmcsydkaos.supabase.co';
-export const supabaseKey = ''; // תצטרך למלא את ה-anon key (public) כאן
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkdmhqcmFpcWlsbWNzeWRrYW9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExODM5OTEsImV4cCI6MjA1Njc1OTk5MX0.m3gYUVysgOw97zTVB8TCqPt9Yf0_3lueAssw3B30miA'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
